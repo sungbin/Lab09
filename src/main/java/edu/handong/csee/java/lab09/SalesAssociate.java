a2eff910edd63890d47187ff00b33463833ff232
sungbin <21700613@handong.edu>
2018-04-11 02:09:33 +0900

    complete SalesAssociate class

---
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -0,0 +1,21 @@
+package edu.handong.csee.java.lab09;
+
+public class SalesAssociate {
+	public String mName;
+	public double mSales;
+	
+	public String getName() {
+		return mName;
+	}
+	public void setName(String name) {
+		this.mName = name;
+	}
+	public double getSales() {
+		return mSales;
+	}
+	public void setSales(double sales) {
+		this.mSales = sales;
+	}
+	
+
+}

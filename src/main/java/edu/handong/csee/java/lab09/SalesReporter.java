b6d147c206acfa403fa6dcfbd696ac03a23123ea
sungbin <21700613@handong.edu>
2018-04-11 13:04:53 +0900

    compete javadoc of SalesReporter

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,72 +1,88 @@
-package edu.handong.csee.java.lab09;
+package edu.handong.csee.java.lab09; //The file is in this package
 
-import java.util.Scanner;
+import java.util.Scanner; //I will use Scanner method
 
-public class SalesReporter {
+/**
+ * This class have Three method :getData, computeStats, displayResults
+ * help getting data, compute data of highestSales and averageSales
+ * Print Result
+ */
+public class SalesReporter { //declare public type class
 
-	public double highestSales;
-	public double averageSales=0;
-	private SalesAssociate[] team;
-	private int numberOfAssociate;
-	Scanner mykeyboard = new Scanner(System.in);
+	public double highestSales; //declare member variable public double type
+	public double averageSales=0; //declare member variable public double type
+	private SalesAssociate[] team; //declare member variable private SalesAssociate[] type, because of arrange
+	private int numberOfAssociate;  //declare member variable private integer type, because the variable is relative with arrange.
+	Scanner mykeyboard = new Scanner(System.in); //make instance of Scanner
 
-	public void getData()
+	/**
+	 * setting data of salesman
+	 */
+	public void getData() //declare public type method
 	{
-		System.out.println("Enter number of sales associates:");
-		this.numberOfAssociate=mykeyboard.nextInt();
-		this.team = new SalesAssociate[numberOfAssociate];
-		for(int i = 0; i <this.numberOfAssociate;i++)
+		System.out.println("Enter number of sales associates:"); //print the sentence
+		this.numberOfAssociate=mykeyboard.nextInt(); //scan one Integer from user
+		this.team = new SalesAssociate[numberOfAssociate]; //define the Arrays length
+		for(int i = 0; i <this.numberOfAssociate;i++) //repeat times of numberOfAssociate value
 		{
-			mykeyboard.nextLine();
-			team[i] = new SalesAssociate();
-			System.out.printf("Enter data for associate number %d\n",i+1);
-			System.out.print("Enter name of sales associate: ");
-			team[i].mName = mykeyboard.nextLine();
-			System.out.print("Enter associate's sales: $");
-			team[i].mSales= mykeyboard.nextDouble();
+			mykeyboard.nextLine(); //prevent error of scan
+			team[i] = new SalesAssociate(); //make instance of SalesAssociate
+			System.out.printf("Enter data for associate number %d\n",i+1); //print sentence
+			System.out.print("Enter name of sales associate: "); //print sentence
+			team[i].mName = mykeyboard.nextLine(); //scan one line from user
+			System.out.print("Enter associate's sales: $"); //print sentence
+			team[i].mSales= mykeyboard.nextDouble(); //scan Double type value
 		}
 	}
 	
-	public void computeStats()
+	/**
+	 * compute the data for average and highest value
+	 * for this, sorting team Array in regular sequence of Sales
+	 */
+	public void computeStats() //declare public type method
 	{
-		SalesAssociate Step;
-		this.highestSales = team[0].mSales;
-		for(int i =0 ;i<this.numberOfAssociate;i++)
+		SalesAssociate Step; //declare SalesAssociate type variable for swapping
+		this.highestSales = team[0].mSales; //put team[0].mSales value to highestSales
+		for(int i =0 ;i<this.numberOfAssociate;i++) //repeat times of numberOfAssociate value
 		{
-			this.averageSales += team[i].mSales;
-			for(int j = 0; j<i ;j++)
+			this.averageSales += team[i].mSales; //plus team[i].mSales value to averageSales
+			for(int j = 0; j<i ;j++) //repeat times of i;
 			{
-				if(team[j].mSales<team[i].mSales)
+				if(team[j].mSales<team[i].mSales) //if team[j].mSales is smaller than team[i].mSales
 				{
-					Step =team[j];
-					team[j]=team[i];
-					team[i]=Step;
+					Step =team[j]; //put team[j] to Step
+					team[j]=team[i]; //put team[i] to team[j]
+					team[i]=Step; //put Step to team[i]
 				}
 			}
 		}
-		this.averageSales /= this.numberOfAssociate;
-		this.highestSales = team[0].mSales;
+		this.averageSales /= this.numberOfAssociate; //calculate average of Sales
+		this.highestSales = team[0].mSales; //put team[0].mSales to highestSales
 	}
-	public void displayResults()
+	
+	/**
+	 * print data of salesman and
+	 * display Results after computing
+	 */
+	public void displayResults() //declare public type method
 	{
-		System.out.println("\nAverage sales per associate is $"+ this.averageSales);
-		System.out.println("The hightest sales figure is $" + this.highestSales);
+		System.out.println("\nAverage sales per associate is $"+ this.averageSales); //print sentence
+		System.out.println("The hightest sales figure is $" + this.highestSales); //print sentence
 		
-		for(int i =0;i<this.numberOfAssociate;i++)
+		for(int i =0;i<this.numberOfAssociate;i++) //repeat times of numberOfAssociate value
 		{
-			if(i==0)
-				System.out.println("\nThe following had the hightest sales:");
-			if(i==1)
-				System.out.println("\nThe rest performed as follows:");
-			System.out.println("Name: "+team[i].mName);
-			System.out.println("Sales: $" + team[i].mSales);
-			if(team[i].mSales-this.averageSales>0)
-				System.out.println("$" + (team[i].mSales-this.averageSales) +" above the arverge.");
-			else if(team[i].mSales-this.averageSales<0)
-				System.out.println("$" + (this.averageSales-team[i].mSales) +" below the arverge.");
-			else
-				System.out.println("The sale equal the averge.");
+			if(i==0) //if i equal 0
+				System.out.println("\nThe following had the hightest sales:"); //print this sentence
+			if(i==1) //if i equal 1
+				System.out.println("\nThe rest performed as follows:"); //print this sentence
+			System.out.println("Name: "+team[i].mName); //print sentence
+			System.out.println("Sales: $" + team[i].mSales); //print sentence
+			if(team[i].mSales-this.averageSales>0) //if team[i].mSales is bigger than averageSales
+				System.out.println("$" + (team[i].mSales-this.averageSales) +" above the arverge."); //print sentence
+			else if(team[i].mSales-this.averageSales<0) //if team[i].mSales is smaller than averageSales
+				System.out.println("$" + (this.averageSales-team[i].mSales) +" below the arverge."); //print sentence
+			else //else,
+				System.out.println("The sale equal the averge."); //print this sentence
 		}
-
 	}
 }

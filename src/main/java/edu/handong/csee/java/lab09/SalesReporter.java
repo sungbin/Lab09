859fc0b4113aa27de036e727d48be9dd4e69f700
sungbin <21700613@handong.edu>
2018-04-20 20:43:24 +0900

    one single line comment of SalesReporter

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,7 +1,7 @@
 package edu.handong.csee.java.lab09; //The file is in this package
 
 import java.util.Scanner; //I will use Scanner method
-import java.util.ArrayList;
+import java.util.ArrayList; //including ArrayList method
 /**
  * This class have Three method :getData, computeStats, displayResults
  * help getting data, compute data of highestSales and averageSales
@@ -11,7 +11,7 @@ public class SalesReporter { //declare public type class
 
 	public double highestSales=0; //declare member variable public double type
 	public double averageSales=0; //declare member variable public double type
-	ArrayList<SalesAssociate> team = new ArrayList<SalesAssociate>();
+	ArrayList<SalesAssociate> team = new ArrayList<SalesAssociate>(); //make SalesAssociate type ArrayList instance, naming team
 	SalesAssociate highest =new SalesAssociate(); // for saving highest instance
 	Scanner mykeyboard = new Scanner(System.in); //make instance of Scanner
 
@@ -23,21 +23,20 @@ public class SalesReporter { //declare public type class
 		/*System.out.println("Enter number of sales associates:"); //print the sentence
 		this.numberOfAssociate=mykeyboard.nextInt(); //scan one Integer from user*/
 		/*this.team = new SalesAssociate[length]; //define the Arrays length*/
-		boolean done = false;
-		while(!done)
+		boolean done = false; //declare boolean type, value is false
+		while(!done) // if done is ture, loop 
 		{
 			SalesAssociate tempotaryTeam = new SalesAssociate(); //declare member variable private SalesAssociate type, because of tempotary saving*/
-			/*team[i] = new SalesAssociate(); //make instance of SalesAssociate*/
 			System.out.print("Enter name of sales associate: "); //print sentence
 			tempotaryTeam.setName(mykeyboard.nextLine());//scan one line from user
 			System.out.print("Enter associate's sales: $"); //print sentence
 			tempotaryTeam.setSales(mykeyboard.nextDouble()); //scan Double type value
 			mykeyboard.nextLine(); //prevent error of scan
-			team.add(tempotaryTeam);
-			System.out.print ("More items for the list? ");
-            String ans = mykeyboard.nextLine ();
-            if (!ans.equalsIgnoreCase ("yes"))
-                done = true;
+			team.add(tempotaryTeam);//add tempotaryTeam to team arraylist
+			System.out.print ("More items for the list? "); //print sentence
+            String ans = mykeyboard.nextLine (); //declare string type, ans
+            if (!ans.equalsIgnoreCase ("yes")) //if ans is not equal yes
+                done = true; // put true to done
 		}
 	}
 	
@@ -47,13 +46,13 @@ public class SalesReporter { //declare public type class
 	 */
 	public void computeStats() //declare public type method
 	{
-		for(SalesAssociate temporaryTeam1 : team)
+		for(SalesAssociate temporaryTeam1 : team) //put team arraylist first index temporaryTeam1, and repeat, while temporaryTeam1 index plus 1
 		{
 			this.averageSales += temporaryTeam1.getSales(); //plus teamporaryTeam.getSales() value to averageSales
-			if(temporaryTeam1.getSales()>highestSales)
+			if(temporaryTeam1.getSales()>highestSales) //if highestSales value bigger than temporaryTeam1's mSales
 			{
-				highestSales = temporaryTeam1.getSales();
-				highest = temporaryTeam1;
+				highestSales = temporaryTeam1.getSales(); //put the value to highestSales
+				highest = temporaryTeam1; //put temporaryTeam1 to highest
 			}
 		}
 		this.averageSales /= team.size(); //calculate average of Sales
@@ -78,10 +77,10 @@ public class SalesReporter { //declare public type class
 		else //else,
 			System.out.println("The sale equal the averge."); //print this sentence
 		System.out.println("\nThe rest performed as follows:"); //print this sentence
-		for(SalesAssociate temporaryTeam1 : team) //repeat times of numberOfAssociate value
+		for(SalesAssociate temporaryTeam1 : team) //put team arraylist first index temporaryTeam1, and repeat, while temporaryTeam1 index plus 1
 		{
-			if(temporaryTeam1.getSales() == highestSales)
-				continue;
+			if(temporaryTeam1.getSales() == highestSales) //if highestSales value equal temporaryTeam1's mSales
+				continue;//ignore one repeat under
 			System.out.println("Name: "+temporaryTeam1.getName()); //print sentence
 			System.out.println("Sales: $" + temporaryTeam1.getSales()); //print sentence
 			if(temporaryTeam1.getSales()-this.averageSales>0) //if temporaryTeam1.getSales() is bigger than averageSales
